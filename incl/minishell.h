deae13462b9538045b1a9120002ea14168e034c7
Severyn Valchuk <svalchuk@student.42wolfsburg.de>
2024-07-04 11:24:06 +0200

    edit struct

--- incl/minishell.h
+++ incl/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asmolnya <asmolnya@student.42.fr>          +#+  +:+       +#+        */
+/*   By: svalchuk <svalchuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 16:06:47 by svalchuk          #+#    #+#             */
-/*   Updated: 2024/07/03 21:43:10 by asmolnya         ###   ########.fr       */
+/*   Updated: 2024/07/04 11:23:40 by svalchuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,7 @@
 
 typedef struct s_cmd
 {
-	// char			*cmd;	// The command name, e.g., "ls"
-	// char			**args;	// An array of arguments, e.g., ["ls", "-l", NULL]
-	char			*cmd;	// An array of command arguments, e.g., ["ls", "-l", NULL]
+	char			**cmd;	// An array of command arguments, e.g., ["ls", "-l", NULL]
 	char			*in_rd;	// Input redirection file, e.g., "input.txt"
 	char			*out_rd;// Output redirection file, e.g., "output.txt"
 	int				append;	// Flag for append mode (1 for >>, 0 for >)
