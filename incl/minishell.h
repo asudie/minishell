c5bc67460db3b28af6042ccab101a0df70fb7208
Severyn Valchuk <svalchuk@student.42wolfsburg.de>
2024-07-04 20:31:30 +0200

    rename struct

--- incl/minishell.h
+++ incl/minishell.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: svalchuk <svalchuk@student.42wolfsburg.de> +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/15 16:06:47 by svalchuk          #+#    #+#             */
-/*   Updated: 2024/07/04 11:23:40 by svalchuk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -39,10 +27,11 @@
 
 typedef struct s_cmd
 {
-	char			**cmd;	// An array of command arguments, e.g., ["ls", "-l", NULL]
+	char			**args;	// An array of command arguments, e.g., ["ls", "-l", NULL]
 	char			*in_rd;	// Input redirection file, e.g., "input.txt"
 	char			*out_rd;// Output redirection file, e.g., "output.txt"
 	int				append;	// Flag for append mode (1 for >>, 0 for >)
+	// HERE-DOC
 	char 			**envp;
 	struct s_cmd	*next;	// Pointer to the next command in case of pipes
 }	t_cmd;
