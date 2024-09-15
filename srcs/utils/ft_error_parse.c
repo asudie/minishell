4dd28494b22bbf053d5e56ce3dfabcffb5e3c4b6
Severyn Valchuk <svalchuk@student.42wolfsburg.de>
2024-09-15 16:11:17 +0200

    norm

--- srcs/utils/ft_error_parse.c
+++ srcs/utils/ft_error_parse.c
@@ -6,7 +6,7 @@
 /*   By: svalchuk <svalchuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 21:38:22 by svalchuk          #+#    #+#             */
-/*   Updated: 2024/09/15 14:59:54 by svalchuk         ###   ########.fr       */
+/*   Updated: 2024/09/15 15:14:10 by svalchuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ bool	ft_input_error(t_mhell *mhell, char	*input)
 	if (!input || ft_is_empty(input))
 		return (false);
 	if (!ft_check_pipe(input) || !ft_unclosed_quotes(input)
-		|| !ft_invalid_chars(input) || /* !ft_pipe_nb(input) || */ !ft_redir(input))
+		|| !ft_invalid_chars(input) || !ft_redir(input))
 		return (mhell->exit_code = 2, false);
 	return (true);
 }
